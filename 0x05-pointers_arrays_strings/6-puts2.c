aa92f2807345127368e770ba2a468c064fbe21cc
ruthkings <you@example.com>
2023-02-27 16:21:28 -0800

    6-puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,6 +10,7 @@ void puts2(char *str)
 int longi = 0;
 int t = 0;
 char *y = str;
+int o;
 while (*y != '\0')
 {
 y++;
@@ -19,7 +20,8 @@ t = longi - 1;
 for (o = 0 ; o <= t ; o++)
 {
 if (o % 2 == 0)
-}_putchar(str[o]);
+{
+_putchar(str[o]);
 }
 }
 _putchar('\n');
